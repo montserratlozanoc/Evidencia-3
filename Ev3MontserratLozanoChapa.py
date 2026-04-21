51c6f69f42195f5576d3464d83acfe5ad2ea318b
André Lozano Chapa <montserrat.lozanoc@uanl.edu.mx>
2026-04-20 19:53:38 -0600

    Se creo la clase Receta para la Evidencia 3

---
+++ Ev3MontserratLozanoChapa.py
@@ -0,0 +1,7 @@
+class Receta:
+    def __init__(self, nombre, dificultad, tiempo_preparacion, calorias_porcion, num_ingredientes):
+        self.__nombre = nombre
+        self.__dificultad = dificultad
+        self.__tiempo_preparacion = tiempo_preparacion
+        self.__calorias_porcion = calorias_porcion
+        self.__num_ingredientes = num_ingredientes
